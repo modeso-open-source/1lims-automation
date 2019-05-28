78ad613b7192d30c3f7c0f4c917eafbf6729a117
Aya <ayaahmed9424@gmail.com>
2019-05-28 10:51:03 +0200

    seperate to two test cases

--- ui_testing/testcases/basic_tests/test03_orders.py
+++ ui_testing/testcases/basic_tests/test03_orders.py
@@ -1,16 +1,17 @@
 from ui_testing.testcases.base_test import BaseTest
 from ui_testing.pages.login_page import Login
 from ui_testing.pages.article_page import Article
-from ui_testing.pages.testplan_page import TestPlan
+from ui_testing.pages.testplan_page import TstPlan
 from ui_testing.pages.order_page import Order
 from ui_testing.pages.analyses_page import Analyses
+
 
 class OrdersTestCases(BaseTest):
     def setUp(self):
         super().setUp()
         self.login_page = Login()
         self.article_page = Article()
-        self.test_plan = TestPlan()
+        self.test_plan = TstPlan()
         self.order_page = Order()
         self.analyses_page = Analyses()
         self.login_page.login(
@@ -18,7 +19,7 @@ class OrdersTestCases(BaseTest):
         self.base_selenium.wait_until_page_url_has(text='dashboard')
         self.order_page.get_orders_page()
 
-    def test01_archiveOrder(self):
+    def test01_archive_order(self):
         """
             New: Orders: Archive
             The user cannot archive an order unless all corresponding analysis are archived
@@ -27,29 +28,45 @@ class OrdersTestCases(BaseTest):
         """
         order_row = self.order_page.get_random_order_row()
         self.order_page.click_check_box(source=order_row)
-        order_row_list = order_row.text.split('\n')
 
-        headers = self.order_page.table_headers()
-        headers = list(filter(lambda x: (x != ''), [
-                       header.text for header in headers]))
-        analysis_index = headers.index('Analysis No.')
-        analysis_numbers_array = order_row_array[analysis_index].split(',')
-
+        analysis_number_value = self.order_page.get_row_cell_text_related_to_header(
+            order_row, 'Analysis No.')
+        analysis_numbers_list = analysis_number_value.split(',')
         order_deleted = self.order_page.archive_selected_orders(
             check_pop_up=True)
-        self.analyses_page.get_analyses_page()
-        self.order_page.sleep_medium()
 
         if order_deleted:
+            self.analyses_page.get_analyses_page()
+            self.order_page.sleep_medium()
             has_active_analysis = self.analyses_page.search_if_analysis_not_deleted(
-                analysis_numbers_array)
+            analysis_numbers_list)
             self.assertEqual(has_active_analysis, False)
-        else:
+
+    def test02_archiveOrder_has_active_analysis(self):
+        """
+            New: Archive order has active analysis
+            The user cannot archive an order unless all corresponding analysis are archived
+            LIMS-4329
+        :return:
+        """
+        order_row = self.order_page.get_random_order_row()
+        self.order_page.click_check_box(source=order_row)
+
+        analysis_number_value = self.order_page.get_row_cell_text_related_to_header(
+            order_row, 'Analysis No.')
+        analysis_numbers_list = analysis_number_value.split(',')
+        order_deleted = self.order_page.archive_selected_orders(
+            check_pop_up=True)
+
+
+        if not order_deleted:
+            self.analyses_page.get_analyses_page()
+            self.order_page.sleep_medium()
             self.analyses_page.search_by_number_and_archive(
-                analysis_numbers_array)
+                analysis_numbers_list)
             self.order_page.get_orders_page()
             self.order_page.sleep_medium()
-            rows = self.order_page.search(analysis_numbers_array[0])
+            rows = self.order_page.search(analysis_numbers_list[0])
             self.order_page.click_check_box(source=rows[0])
             self.order_page.archive_selected_orders()
             rows = self.order_page.result_table()
