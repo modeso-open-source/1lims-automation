8ac2e6025a79983c8637a313c634013ac226d4f9
0xislamtaha <islamtaha2012@gmail.com>
2019-06-26 17:25:55 +0200

    gonna remove it

--- ui_testing/pages/login_page.py
+++ ui_testing/pages/login_page.py
@@ -7,6 +7,7 @@ class Login:
 
     def login(self, username, password):
         self.base_selenium.get(url=self.base_selenium.url)
+        print(self.base_selenium.driver.find_element_by_xpath('//*[@id="m_login"]/div[1]/div/div/div/div[2]/div/h3').text)
         self.base_selenium.set_text(element='login:username', value=username)
         self.base_selenium.set_text(element='login:password', value=password)
         self.base_selenium.click(element='login:login_btn')
