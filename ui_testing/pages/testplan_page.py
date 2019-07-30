0bc38eff403ed2364c172bd70d478390bfc1314f
0xIslamTaha <islamtaha2012@gmail.com>
2019-07-30 19:46:42 +0200

    fixs

--- ui_testing/pages/testplan_page.py
+++ ui_testing/pages/testplan_page.py
@@ -47,7 +47,7 @@ class TstPlan(TestPlans):
     def set_test_unit(self, test_unit='', **kwargs):
         self.base_selenium.click('test_plan:next')
         self.base_selenium.click('test_plan:add_test_units')
-        self.base_selenium.select_item_from_drop_down(element='test_plan:test_units', item_text='Qualitative')
+        self.base_selenium.select_item_from_drop_down(element='test_plan:test_units', item_text=test_unit)
         self.base_selenium.click('test_plan:add')
 
     def create_new_test_plan(self, name='', material_type='', article='', test_unit='', **kwargs):
