0657f5560661d115ede30de0b3460b74b51965b9
mazenmmbadr <mazen.mohamed@modeso.ch>
2019-12-18 16:51:14 +0200

    changing function name in testcase

--- ui_testing/testcases/basic_tests/test08_contacts.py
+++ ui_testing/testcases/basic_tests/test08_contacts.py
@@ -21,7 +21,7 @@ class ContactsTestCases(BaseTest):
         """
         
         self.base_selenium.LOGGER.info('Creating new contact')
-        contact_data = self.contact_page.create_new_contact()
+        contact_data = self.contact_page.create_update_contact()
 
         self.base_selenium.LOGGER.info('comparing contact\'s data with the first record in contact page')
         self.base_selenium.LOGGER.info('to make sure that when new record is created is set to the be the first record in the page')
