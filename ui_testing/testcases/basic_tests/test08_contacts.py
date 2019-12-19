d8eaf8c0117c96a0e7ce82a0ecb53e847bd5fb66
mazenmmbadr <mazen.mohamed@modeso.ch>
2019-12-19 11:57:42 +0200

    adding archive/ restore function

--- ui_testing/testcases/basic_tests/test08_contacts.py
+++ ui_testing/testcases/basic_tests/test08_contacts.py
@@ -11,6 +11,35 @@ class ContactsTestCases(BaseTest):
         self.base_selenium.wait_until_page_url_has(text='dashboard')
         self.contact_page.get_contacts_page()
 
+    @parameterized.expand(['archive', 'restore'])
+    def test_001_archive_restore_contact(self, action):
+        """
+        New: Contact: Restore/Archive Approach: I can archive/restore any contact successfully
+        I can archive/restore any contact successfully
+        LIMS-3566
+        """
+        
+        if action == 'restore':
+            self.contact_page.get_archived_contacts()
+
+        selected_contacts_data, _ = self.contact_page.select_random_multiple_table_rows()
+        if action == 'archive':
+            self.contact_page.archive_selected_contacts()
+        elif action == 'restore':
+            self.contact_page.restore_selected_contacts()
+        if action == 'archive':
+            self.contact_page.get_archived_contacts()
+        elif action == 'restore':
+            self.contact_page.get_active_contacts()
+
+        for contact in selected_contacts_data:
+            contact_no = contact['Contact No']
+            if action == 'archive':
+                self.base_selenium.LOGGER.info(' + {} Contact should be archived.'.format(contact_no))
+            elif action == 'restore':
+                self.base_selenium.LOGGER.info(' + {} Contact should be active.'.format(contact_no))
+            self.assertTrue(self.contact_page.is_contact_in_table(value=contact_no))
+
     
     def test_004_search_by_any_field(self):
         """
