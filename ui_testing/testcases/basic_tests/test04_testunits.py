f97221e375c2a83bee12378f7cc44f1ab8a35262
ghada-37 <grizk37@gmail.com>
2020-01-22 10:51:26 +0200

    no need to search for empty elements

--- ui_testing/testcases/basic_tests/test04_testunits.py
+++ ui_testing/testcases/basic_tests/test04_testunits.py
@@ -13,20 +13,20 @@ class TestUnitsTestCases(BaseTest):
         self.base_selenium.wait_until_page_url_has(text='dashboard')
         self.test_unit_page.get_test_units_page()
 
-    @skip('https://modeso.atlassian.net/browse/LIMS-5237')
+    #@skip('https://modeso.atlassian.net/browse/LIMS-5237')
     def test001_test_units_search(self):
         """
         New: Test units: Search Approach: I can search by any field in the table view
 
         LIMS-3674
-        :return:
         """
         row = self.test_unit_page.get_random_test_units_row()
         row_data = self.base_selenium.get_row_cells_dict_related_to_header(row=row)
         for column in row_data:
-            if re.findall(r'\d{1,}.\d{1,}.\d{4}', row_data[column]) or row_data[column] == '':
+            if re.findall(r'\d{1,}.\d{1,}.\d{4}', row_data[column]) \
+                    or row_data[column] == '-' or not (row_data[column]) or row_data[column] == 'N/A':
                 continue
-            self.base_selenium.LOGGER.info(' + search for {} : {}'.format(column, row_data[column]))
+            self.info(' + search for {} : {}'.format(column, row_data[column]))
             search_results = self.article_page.search(row_data[column])
             self.assertGreater(len(search_results), 1, " * There is no search results for it, Report a bug.")
             for search_result in search_results:
