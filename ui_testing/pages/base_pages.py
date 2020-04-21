49d1afc18953de97cb47f9cac457773ee6d5d2aa
ghada-37 <grizk37@gmail.com>
2020-04-21 20:56:43 +0200

    added time sleep

--- ui_testing/pages/base_pages.py
+++ ui_testing/pages/base_pages.py
@@ -92,6 +92,7 @@ class BasePages:
 
     def filter_by_date(self, first_filter_element, first_filter_text, second_filter_element, second_filter_text):
         self.open_filter_menu()
+        self.sleep_small()
         self.base_selenium.wait_element(element=first_filter_element)
         self.base_selenium.set_text(element=first_filter_element, value=first_filter_text)
         self.base_selenium.set_text(element=second_filter_element, value=second_filter_text)
