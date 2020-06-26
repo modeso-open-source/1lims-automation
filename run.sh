811c15ebe973312170f414e40fbd8f97c03af8bd
0xIslamTaha <islamtaha2012@gmail.com>
2020-06-26 12:13:26 +0200

    dockerization

---
+++ run.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+export color_prompt=yes
+cd 1lims-automation;
+export PYTHONPATH='$PYTHONPATH:/1lims-automation';
+export LOGURU_LEVEL='DEBUG';
+xvfb-run -a nosetests -vs --nologcapture --tc-file=config.ini --with-flaky --force-flaky --max-runs=3 --min-passes=1 -m 'test002' ui_testing/testcases/basic_tests/test06_orders.py:OrdersTestCases;
